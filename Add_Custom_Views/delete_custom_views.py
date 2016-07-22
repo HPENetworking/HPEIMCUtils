6f5106593a37a49f89d58930d7106771a593db40
netmanchris <chris_young@me.com>
2016-07-22 15:09:11 -0400

    Added Add_Custom_Views folder with scripts to import and delete custom views based on the contents of the custom_views.csv file

---
+++ Add_Custom_Views/delete_custom_views.py
@@ -0,0 +1,45 @@
+
+
+#!/usr/bin/env python3
+# author: @netmanchris
+
+"""
+Copyright 2016 Hewlett Packard Enterprise Development LP.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+""" This file will take the contents ofcustom_views.csv as input and automatically delete teh corresponding custom views
+ in the HPE IMC Network Management System
+
+ This library uses the pyhpeimc python wrapper around the IMC RESTful API to automatically push the new performance tasks
+ with minimal effort on the part of the user."""
+
+
+import csv
+import time
+from pyhpeimc.auth import *
+from pyhpeimc.plat.groups import *
+
+
+
+auth = IMCAuth("http://", "10.101.0.203", "8080", "admin", "admin")
+
+
+with open('custom_views.csv') as csvfile:
+        # decodes file as csv as a python dictionary
+        reader = csv.DictReader(csvfile)
+        for view in reader:
+            # loads each row of the CSV as a JSON string
+            name = view['name']
+            delete_custom_view(url=auth.url, auth=auth.creds, name=name)
