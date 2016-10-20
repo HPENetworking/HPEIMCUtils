55555eef281d691c8284761ae30932f991a0073a
fgiraud26 <fabien1.giraud@gmail.com>
2016-10-20 16:34:33 +0200

    Added comment in script
    Added some comments for documentation (and also renamed the script since
    there was a type mistake)

--- PythonUtilities/Export_Alarms/exportAlarm.py
+++ PythonUtilities/Export_Alarms/exportAlarm.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
+# author: @netmanFabe
 
+"""
+Copyright 2016 Hewlett Packard Enterprise Development LP.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+__author__ = "@netmanfabe"
+__copyright__ = "Copyright 2016, Hewlett Packard Enterprise Development LP."
+__credits__ = ["Fabien GIRAUD"]
+__license__ = "Apache2"
+__version__ = "2.0.0"
+__maintainer__ = "Fabien GIRAUD"
+__email__ = "fabien_giraud@me.com"
+__status__ = "Prototype"
+
+"""
 import csv
 from pyhpeimc.auth import *
 import time
 import datetime
-#from pyhpeimc.plat.alarm import *
-
-HEADERS = {'Accept': 'application/json', 'Content-Type':
-    'application/json', 'Accept-encoding': 'application/json'}
 
 
 headers = {'Accept': 'application/json', 'Content-Type':
@@ -15,6 +37,14 @@ headers = {'Accept': 'application/json', 'Content-Type':
 
 authIMC = IMCAuth("http://", "192.168.20.21", "8080", "imcrs", "imcrs")
 
+# Function to export alarms using IMC API filtered by recovery status and received time
+#	Parameters:
+#			username: IMC operator name which has access to the alarms
+#			auth: IMC authentication credentials
+#			url: IMC URL
+#			rec: recovery status (0: not recovered ; 1: recovered)
+#			startTime: received alarm time filter (only get alarm after startTime)
+#	Returns a list of alarm (list of json items)
 def export_alarms(username,auth, url, rec,startTime):
 
 	timestamp_startTime = time.mktime(datetime.datetime.strptime(startTime, "%d/%m/%Y %H:%M:%S").timetuple())
@@ -30,7 +60,12 @@ def export_alarms(username,auth, url, rec,startTime):
 	except requests.exceptions.RequestException as e:
 			return "Error:\n" + str(e) + ' get_alarms: An Error has occured'
 
-			
+
+# Function to filter a list of alarm based on recovery time
+#	Parameters:
+#			alarm_list: list of alarm to filter (list of json items)
+#			dateTime_begin: date Time filter for recovery time (keep only alarms recovered after this)
+#	Returns a list of alarm (list of json items)
 def filter_by_rectime(alarm_list,dateTime_begin):
 	"""
 	Function to filter a list of alarm  in order to keep alarms recovered after dateTime_begin
@@ -45,6 +80,11 @@ def filter_by_rectime(alarm_list,dateTime_begin):
 	
 	return alarm_list_filtered
 
+# Function to convert json list to csv file
+#	Parameters:
+#			list: list of json object to convert
+#			filename: file in which to write
+#			delimiter: csv delimiter
 def write_json_to_csv(list,filename,delimiter):
 
 	with open(filename, "w") as file:
@@ -53,6 +93,7 @@ def write_json_to_csv(list,filename,delimiter):
 		csv_file.writeheader()
 		csv_file.writerows(list)
 
+# I used fixed parameters for recstatus, receivedtime and rectime, but we can also use command line arguments ...
 fulllist = export_alarms('admin',authIMC.creds,authIMC.url,'1','19/09/2016 10:00:00')
 #print("Hello %s" %(list))
 filteredList = filter_by_rectime(fulllist,'20/09/2016 10:00:00')
